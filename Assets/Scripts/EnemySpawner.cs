7c0c0e59128d5501f191a1c963401f1185f9df3c
Hamza HERBOU <hamza95herbou@gmail.com>
2021-09-03 11:27:40 +0100

    first commit

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine ;
+
+[System.Serializable]
+public class Enemy {
+   // for debug :
+   public string Name ;
+
+   public GameObject Prefab ;
+   [Range (0f, 100f)]public float Chance = 100f ;
+
+   [HideInInspector] public double _weight ;
+}
+
+
+public class EnemySpawner : MonoBehaviour {
+   [SerializeField] private Enemy[] enemies ;
+
+   private double accumulatedWeights ;
+   private System.Random rand = new System.Random () ;
+
+
+   private void Awake () {
+      CalculateWeights () ;
+   }
+
+
+   private void Start () {
+      // Test: Spawn 200 enemy : 
+      for (int i = 0; i < 200; i++)
+         SpawnRandomEnemy (new Vector2 (Random.Range (-3f, 3f), Random.Range (-4f, 4f))) ;
+   }
+
+   private void SpawnRandomEnemy (Vector2 position) {
+      Enemy randomEnemy = enemies [ GetRandomEnemyIndex () ] ;
+
+      Instantiate (randomEnemy.Prefab, position, Quaternion.identity, transform) ;
+
+      // This line is not required (debug) :
+      Debug.Log ("<color=" + randomEnemy.Name + ">●</color> Chance: <b>" + randomEnemy.Chance + "</b>%") ;
+   }
+
+   private int GetRandomEnemyIndex () {
+      double r = rand.NextDouble () * accumulatedWeights ;
+
+      for (int i = 0; i < enemies.Length; i++)
+         if (enemies [ i ]._weight >= r)
+            return i ;
+
+      return 0 ;
+   }
+
+   private void CalculateWeights () {
+      accumulatedWeights = 0f ;
+      foreach (Enemy enemy in enemies) {
+         accumulatedWeights += enemy.Chance ;
+         enemy._weight = accumulatedWeights ;
+      }
+   }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Unity Tutorial - Spawn random enemies with probabilities
+
+### Video tutorial : [https://youtu.be/Hy6Gxtk0QwY](https://youtu.be/Hy6Gxtk0QwY)
+
+![Video thumbnail](https://img.youtube.com/vi/Hy6Gxtk0QwY/0.jpg)
+
+
+
+
+<br><br><br><br>
+
+## ❤️ Donate
+
+
+
+<a href="https://paypal.me/hamzaherbou" title="https://paypal.me/hamzaherbou" target="_blank"><img align="left" height="50" src="https://www.mediafire.com/convkey/72dc/iz78ys7vtfsl957zg.jpg" alt="Paypal"></a>
+
+
+
+<a href="https://www.buymeacoffee.com/hamzaherbou" title="https://www.buymeacoffee.com/hamzaherbou" target="_blank"><img align="left" height="50" src="https://www.mediafire.com/convkey/66bc/dg3xdk96km1pt7gzg.jpg" alt="BuyMeACoffee"></a>
+
+
+
+<a href="https://patreon.com/herbou" title="https://patreon.com/herbou" target="_blank"><img align="left" height="50" src="https://www.mediafire.com/convkey/57b1/0h171bqmdesoljczg.jpg" alt="Patreon"></a>
